b0e25c17726dd3a0fa9893c0d5177bfea6e01268
Danilo Da Silva Moreira Pires <danilolex5@gmail.com>
2025-04-09 20:48:00 -0300

    Simplificando 2
    Manter o Main no inicio do código.

--- teste/lib/main.dart
+++ teste/lib/main.dart
@@ -2,83 +2,6 @@ import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
-class Game {
-  String id;
-  String nome;
-  int quantidade;
-  double preco;
-
-  Game({required this.id, required this.nome, required this.quantidade, required this.preco});
-
-  factory Game.fromJson(Map<String, dynamic> json) {
-    return Game(
-      id: json['id'],
-      nome: json['nome'],
-      quantidade: json['quantidade'],
-      preco: (json['preco'] as num).toDouble(),
-    );
-  }
-
-  Map<String, dynamic> toJson() {
-    return {
-      'id': id,
-      'nome': nome,
-      'quantidade': quantidade,
-      'preco': preco,
-    };
-  }
-}
-
-const String baseUrl = 'https://67f6d50c42d6c71cca636c42.mockapi.io/ap1/v1/games';
-
-Future<List<Game>> getGames() async {
-  final response = await http.get(Uri.parse(baseUrl));
-  if (response.statusCode == 200) {
-    List<dynamic> data = json.decode(response.body);
-    return data.map((json) => Game.fromJson(json)).toList();
-  } else {
-    throw Exception('Erro ao carregar jogos');
-  }
-}
-
-Future<Game> createGame(Game game) async {
-  final response = await http.post(
-    Uri.parse(baseUrl),
-    headers: {"Content-Type": "application/json"},
-    body: json.encode({
-      'nome': game.nome,
-      'quantidade': game.quantidade,
-      'preco': game.preco,
-    }),
-  );
-
-  if (response.statusCode == 201) {
-    return Game.fromJson(json.decode(response.body));
-  } else {
-    throw Exception('Erro ao criar jogo: ${response.statusCode} ${response.body}');
-  }
-}
-
-Future<Game> updateGame(Game game) async {
-  final url = Uri.parse('$baseUrl/${game.id}');
-  final response = await http.put(
-    url,
-    headers: {"Content-Type": "application/json"},
-    body: json.encode(game.toJson()),
-  );
-  if (response.statusCode == 200) {
-    return Game.fromJson(json.decode(response.body));
-  } else {
-    throw Exception('Erro ao atualizar jogo: ${response.statusCode} ${response.body}');
-  }
-}
-
-Future<void> deleteGame(String id) async {
-  final response = await http.delete(Uri.parse('$baseUrl/$id'));
-  if (response.statusCode != 200) {
-    throw Exception('Erro ao deletar jogo');
-  }
-}
 
 
 void main() {
@@ -332,5 +255,83 @@ class ItemListaGames extends StatelessWidget {
         ),
       ),
     );
+  }
+}
+
+class Game {
+  String id;
+  String nome;
+  int quantidade;
+  double preco;
+
+  Game({required this.id, required this.nome, required this.quantidade, required this.preco});
+
+  factory Game.fromJson(Map<String, dynamic> json) {
+    return Game(
+      id: json['id'],
+      nome: json['nome'],
+      quantidade: json['quantidade'],
+      preco: (json['preco'] as num).toDouble(),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'nome': nome,
+      'quantidade': quantidade,
+      'preco': preco,
+    };
+  }
+}
+
+const String baseUrl = 'https://67f6d50c42d6c71cca636c42.mockapi.io/ap1/v1/games';
+
+Future<List<Game>> getGames() async {
+  final response = await http.get(Uri.parse(baseUrl));
+  if (response.statusCode == 200) {
+    List<dynamic> data = json.decode(response.body);
+    return data.map((json) => Game.fromJson(json)).toList();
+  } else {
+    throw Exception('Erro ao carregar jogos');
+  }
+}
+
+Future<Game> createGame(Game game) async {
+  final response = await http.post(
+    Uri.parse(baseUrl),
+    headers: {"Content-Type": "application/json"},
+    body: json.encode({
+      'nome': game.nome,
+      'quantidade': game.quantidade,
+      'preco': game.preco,
+    }),
+  );
+
+  if (response.statusCode == 201) {
+    return Game.fromJson(json.decode(response.body));
+  } else {
+    throw Exception('Erro ao criar jogo: ${response.statusCode} ${response.body}');
+  }
+}
+
+Future<Game> updateGame(Game game) async {
+  final url = Uri.parse('$baseUrl/${game.id}');
+  final response = await http.put(
+    url,
+    headers: {"Content-Type": "application/json"},
+    body: json.encode(game.toJson()),
+  );
+  if (response.statusCode == 200) {
+    return Game.fromJson(json.decode(response.body));
+  } else {
+    throw Exception('Erro ao atualizar jogo: ${response.statusCode} ${response.body}');
+  }
+}
+
+Future<void> deleteGame(String id) async {
+  final response = await http.delete(Uri.parse('$baseUrl/$id'));
+  if (response.statusCode != 200) {
+    throw Exception('Erro ao deletar jogo');
   }
 }
